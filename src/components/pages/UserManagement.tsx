165ba70407cf4c4f4b3ad7a0916c9ac1bfd64eb5
kentakki416 <fujiken0814@outlook.jp>
2022-06-27 02:33:14 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,32 @@
-import { memo, FC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps*/
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, FC, useEffect } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
+    const { getUsers, users, loading} = useAllUsers();
+
+    useEffect(()=> getUsers(), []);
     return (
-        <p>ユーザー管理ページです</p>
+        <>
+            {loading ? (
+                <Center>
+                    <Spinner/>
+                </Center>
+                ):(
+                    <Wrap p={{base:4, md:10}}>
+                        {users.map((user) => (
+                            <WrapItem key={user.id} mx="auto">
+                                <UserCard 
+                                    imageUrl="https://source.unsplash.com/random"
+                                    userName={user.username}
+                                    fullName={user.name}
+                                />
+                            </WrapItem>
+                        ))}
+                    </Wrap>
+                )}
+        </>
     );
 });
