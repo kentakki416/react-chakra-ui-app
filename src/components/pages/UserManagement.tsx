e77a6edb24dd37cf5913d434d6d34a8a69dfa18b
kentakki416 <fujiken0814@outlook.jp>
2022-06-28 00:46:29 +0900

    ユーザー詳細モーダル機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps*/
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, FC, useEffect } from "react";
+import { Center,  Spinner,  useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, FC, useEffect, useCallback } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: FC = memo(() => {
+    const { isOpen, onOpen, onClose} = useDisclosure();
     const { getUsers, users, loading} = useAllUsers();
+    const { onSelectUser, selectedUser } = useSelectUser();
 
     useEffect(()=> getUsers(), []);
+
+    const onClickUser = useCallback((id: number) => {
+        onSelectUser({ id, users, onOpen});
+    },[users]);
+
     return (
         <>
             {loading ? (
@@ -19,14 +28,18 @@ export const UserManagement: FC = memo(() => {
                         {users.map((user) => (
                             <WrapItem key={user.id} mx="auto">
                                 <UserCard 
+                                    id={user.id}
                                     imageUrl="https://source.unsplash.com/random"
                                     userName={user.username}
                                     fullName={user.name}
+                                    onClick={onClickUser}
                                 />
                             </WrapItem>
                         ))}
                     </Wrap>
-                )}
+                )
+            }
+            <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose}/>
         </>
     );
 });
